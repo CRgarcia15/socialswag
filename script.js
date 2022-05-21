e150e7b2150b4d802c1274544c25f2d534800205
Raymond Mojica <rm@M12020.home>
2022-05-21 01:50:31 -0400

    formHidden and JS

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-var x = document.getElementById("myText").value;
+/* Once the document is ready to be worked with we can run a function and get a reference to both Login and Sign Up forms*/
+document.addEventListener("DOMContentLoaded", () =>{
+    const loginForm = document.querySelector("#login");
+    const createAccountForm = document.querySelector("#createAccount");
+
+    /* When clicked on the create account it will hide the login for and remove the formHidden from the createAccountForm */
+    document.querySelector("#linkCreateAccount").addEventListener("click", e =>{ /* e will pass a reference in place of () in the Event */
+        e.preventDefault(); /* This will prevent the html from redirecting and loading webpages from the href*/
+        loginForm.classList.add("formHidden");
+        createAccountForm.classList.remove("formHidden");
+    });
+
+ /* Same code as above but the other way around so when the Login form is cliked the createAccountForm is hidden */
+    document.querySelector("#linkLogin").addEventListener("click", e  =>{ /* e will pass a reference in place of () in the Event*/
+        e.preventDefault();
+        loginForm.classList.remove("formHidden");
+        createAccountForm.classList.add("formHidden");
+    });
+});
